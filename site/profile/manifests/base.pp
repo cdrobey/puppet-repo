874e3298a90310fbc4fdfb821a66be6c9c555e99
cdrobey <cdroberson@gmail.com>
2017-05-18 10:29:25 -0600

    Built time base profile

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,11 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  class profile::base::time {
+    include ::ntp
+    file { '/etc/localtime':
+      ensure => link,
+      target => '/usr/share/zoneinfo/US/Central',
+    }
+  }
 }
