66f0158849e403534046198a9658660dd1def55c
cdrobey <cdroberson@gmail.com>
2017-05-20 12:30:04 -0600

    Replaced accounts with different Puppet Module and migrated ssh data to Hiera.

--- site/profile/manifests/base/accounts.pp
+++ site/profile/manifests/base/accounts.pp
@@ -1,14 +1,17 @@
 # == Class: profile::base::accounts
 class profile::base::accounts {
-#  class { 'accounts':
-#    ssh_keys       => hiera_hash('accounts::ssh_keys', {}),
-#    users          => hiera_hash('accounts::users', {}),
-#    usergroups     => hiera_hash('accounts::usergroups', {}),
-#    purge_ssh_keys => true,
-#  }
-  class { '::accounts':
-    ssh_keys   => hiera_hash('accounts::ssh_keys', {}),
-    users      => hiera_hash('accounts::users', {}),
-    usergroups => hiera_hash('accounts::usergroups', {}),
+  user {'ubuntu':
+    ensure  => 'present',
+    shell   => '/usr/bin/zsh',
+    comment => 'root',
+    home    => '/home/ubuntu',
+  }
+
+  class { 'accounts':
+    ssh_keys       => hiera_hash('accounts::ssh_keys'),
+    users          => hiera_hash('accounts::users'),
+    usergroups     => hiera_hash('accounts::usergroups'),
+    purge_ssh_keys => true,
+    require        => Package['zsh'],
   }
 }
