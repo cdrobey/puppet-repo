d1dde774855b04737ff2611e860ce2f9ba95523b
cdrobey <cdroberson@gmail.com>
2017-05-24 08:19:34 -0600

    Changed apt module/manifests to app for generic support.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,10 +1,7 @@
 # == Class: profile::base
 class profile::base {
   include 'profile::base::accounts'
-
-  if $::os['family'] == 'Ubuntu'{
-    include 'profile::base::apt'
-  }
+  include 'profile::base::app'
   include 'profile::base::motd'
   include 'profile::base::ssh'
   include 'profile::base::sudoers'
