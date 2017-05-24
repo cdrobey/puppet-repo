8500907dea7104216a0d154b640415658686c87e
cdrobey <cdroberson@gmail.com>
2017-05-24 03:13:32 -0600

    Updated check for apt.  Ensures module runs for non-Ubuntu OSs.

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,10 @@
 # == Class: profile::base
 class profile::base {
   include 'profile::base::accounts'
-  include 'profile::base::apt'
+
+  if $::os['family'] == 'Ubuntu'{
+    include 'profile::base::apt'
+  }
   include 'profile::base::motd'
   include 'profile::base::ssh'
   include 'profile::base::sudoers'
