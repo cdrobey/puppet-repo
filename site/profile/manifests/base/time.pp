53a9e81dff5de835a3df852b5f4ab88a3f22ec79
cdrobey <cdroberson@gmail.com>
2017-06-15 18:59:04 -0700

    Removed motd .epp

--- site/profile/manifests/base/time.pp
+++ site/profile/manifests/base/time.pp
@@ -2,9 +2,11 @@
 class profile::base::time {
 
   $time_tz = hiera('profile::base::time')
+  $time_ntp_servers = hiera_array('profile::base::ntpservers')
 
   class { 'ntp':
     package_ensure => 'present',
+    servers        => $time_ntp_servers,
   }
 
   file { '/etc/localtime':
