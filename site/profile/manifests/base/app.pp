01f15d3b2bd154e666786be2982e9e22aed71959
cdrobey <cdroberson@gmail.com>
2017-06-21 09:08:18 -0600

    Upgrading Profile app.pp for heira access.

--- site/profile/manifests/base/app.pp
+++ site/profile/manifests/base/app.pp
@@ -1,14 +1,13 @@
 # == Class: profile::base::apt
-class profile::base::app {
-
-  # Hiera lookups
+class profile::base::app (
   $update_frequency = hiera('profile::base::app::update_frequency')
-  $packages = hiera_array('profile::base::app::packages')
+  $packages = hiera_array('profile::base::app::packages'),
+) {
 
   if $facts['os']['family'] == 'Debian'{
     class { 'apt':
       update => {
-      frequency => $update_frequency,
+        frequency => $update_frequency,
       },
     }
   }
