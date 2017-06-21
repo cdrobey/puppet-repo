19a6c3796a54db5a22e50ec47135e2c60aa49eff
cdrobey <cdroberson@gmail.com>
2017-06-21 14:01:10 -0600

    Trying to figure the right way to address a sub-class

--- site/profile/manifests/base/sudoers.pp
+++ site/profile/manifests/base/sudoers.pp
@@ -10,8 +10,6 @@ class profile::base::sudoers (
     purge               => $purge,
     config_file_replace => $config_file_replace,
     includedirsudoers   => $includedirsudoers,
-  }
-  sudo::configs {
-    configs_hash => $configs,
+    conf                => $configs
   }
 }
