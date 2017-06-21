7b7eea1b52584e306d4e147e5189b42f7d1c9aea
cdrobey <cdroberson@gmail.com>
2017-06-21 16:17:35 -0600

    Refactored ssh.

--- site/profile/manifests/base/ssh.pp
+++ site/profile/manifests/base/ssh.pp
@@ -7,14 +7,10 @@ class profile::base::ssh (
 ) {
 
 
-  class { 'ssh::server':
-    options => {
-      sshd_password_authentication     => $password_authentication,
-      permit_root_login                => $permit_root_login,
-      sshd_config_permitemptypasswords => $permit_empty_passwords,
-      manage_firewall                  => $manage_firewall,
-
-
-    }
+  class { 'ssh':
+    sshd_password_authentication     => $password_authentication,
+    permit_root_login                => $permit_root_login,
+    sshd_config_permitemptypasswords => $permit_empty_passwords,
+    manage_firewall                  => $manage_firewall,
   }
 }
