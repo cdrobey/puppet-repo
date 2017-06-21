b34bf24e87a6299f9614f9bcd88f359c83d682c0
cdrobey <cdroberson@gmail.com>
2017-06-21 16:15:46 -0600

    linting.

--- site/profile/manifests/base/ssh.pp
+++ site/profile/manifests/base/ssh.pp
@@ -12,7 +12,7 @@ class profile::base::ssh (
       sshd_password_authentication     => $password_authentication,
       permit_root_login                => $permit_root_login,
       sshd_config_permitemptypasswords => $permit_empty_passwords,
-      manage_firewall                   => $manage_firewall,
+      manage_firewall                  => $manage_firewall,
 
 
     }
