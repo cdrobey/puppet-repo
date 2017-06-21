471ac20af8e0364921dedf990bf78cb8baec5540
cdrobey <cdroberson@gmail.com>
2017-06-21 09:53:12 -0600

    Updated ticks in code.

--- site/profile/manifests/base/fw.pp
+++ site/profile/manifests/base/fw.pp
@@ -2,8 +2,8 @@
 class profile::base::fw {
 
   class { 'firewall:'}
-  class { 'profile::base::fwpre: '}
-  class { 'profile::base::fwpost: '}
+  class { 'profile::base::fwpre:' }
+  class { 'profile::base::fwpost:' }
 
   Firewall {
     before  => Class['profile::base::fwpost'],
