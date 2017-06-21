b2f41e1efba87b3cf79605c3bdabc27faf2839fd
cdrobey <cdroberson@gmail.com>
2017-06-21 09:40:22 -0600

    Updated variable names to match yaml

--- site/profile/manifests/base/time.pp
+++ site/profile/manifests/base/time.pp
@@ -1,16 +1,16 @@
 # == Class: profile::base::time
 class profile::base::time (
-  $time_tz,
-  $time_ntp_servers,
+  $timezone,
+  $ntp_servers,
 ) {
 
   class { 'ntp':
     package_ensure => 'present',
-    servers        => $time_ntp_servers,
+    servers        => $ntp_servers,
   }
 
   file { '/etc/localtime':
     ensure => link,
-    target => $time_tz,
+    target => $timezone,
   }
 }
