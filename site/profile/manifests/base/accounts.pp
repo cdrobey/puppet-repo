d73c9df6c4e74746e04d905fcd97f759c15f1e7c
cdrobey <cdroberson@gmail.com>
2017-06-21 10:40:19 -0600

    Refactoring accounts profile to support profile standards.

--- site/profile/manifests/base/accounts.pp
+++ site/profile/manifests/base/accounts.pp
@@ -1,4 +1,10 @@
 # == Class: profile::base::accounts
-class profile::base::accounts {
-  include accounts
+class profile::base::accounts (
+  $users,
+  $user_defaults,
+  ) {
+  class { 'accounts:'
+    user_defaults => $user_defaults,
+    users         => $users,
+  }
 }
