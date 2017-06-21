5154782c39bea7663af3fbc71c3d7a5420449c1d
cdrobey <cdroberson@gmail.com>
2017-06-21 09:26:23 -0600

    Continuing to refine paramters for the app profile class.

--- site/profile/manifests/base/app.pp
+++ site/profile/manifests/base/app.pp
@@ -1,7 +1,7 @@
 # == Class: profile::base::apt
 class profile::base::app (
-  $update_frequency = hiera('profile::base::app::update_frequency')
-  $packages = hiera_array('profile::base::app::packages'),
+  $update_frequency,
+  $packages,
 ) {
 
   if $facts['os']['family'] == 'Debian'{
