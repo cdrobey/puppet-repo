f02edd1fc83afcec92eebcd4cae2c1497208d67e
cdrobey <cdroberson@gmail.com>
2017-06-21 11:28:06 -0600

    Refactoring ssh to support best practices.

--- site/profile/manifests/base/ssh.pp
+++ site/profile/manifests/base/ssh.pp
@@ -1,8 +1,8 @@
 # == Class: profile::base::ssh
-class profile::base::ssh {
-
-  $server_options_passwordauth = hiera('profile::base::ssh::server_options::passwordauth')
-  $server_options_permitrootlogin = hiera('profile::base::ssh::server_options::permitrootlogin')
+class profile::base::ssh (
+  $passwordauth,
+  $permitrootlogin,
+) {
 
   firewall { '100 allow ssh access':
       dport  => '22',
@@ -10,8 +10,12 @@ class profile::base::ssh {
       action => accept,
   }
 
-  class { 'ssh::server_options':
-    PasswordAuthentication => $server_options_passwordauth,
-    PermitRootLogin        => $server_options_permitrootlogin,
+  class { 'ssh': }
+
+  class { 'ssh::server':
+    options => {
+      'PasswordAuthentication' => $passwordauth,
+      'PermitRootLogin'        => $permitrootlogin,
+    },
   }
 }
