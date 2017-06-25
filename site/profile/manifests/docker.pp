cbc1b2d059d111ea3b3cc88eb8697ad3b3e56828
Chris Roberson <chris.roberson@chris.roberson-C02Q50FTFVH8>
2017-06-24 19:33:12 -0600

    Debugging docker run.

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -11,7 +11,7 @@ class profile::docker
     ports            => ['3478', '6789', 8080, 8443, 8880, 8843],
     expose           => ['3478', '6789', 8080, 8443, 8880, 8843],
     volumes          => ['/var/lib/couchdb', '/var/log'],
-    volumes_to       => [ '/var/lib/unifi', '/var/log/unifi' ],
+    volumes_from     => [ '/var/lib/unifi', '/var/log/unifi' ],
     restart_service  => true,
     extra_parameters => [ '--restart=always' ],
   }
