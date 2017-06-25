00ecb954e912bafa61f971d9feb1fc74df4f3bf8
Chris Roberson <chris.roberson@chris.roberson-C02Q50FTFVH8>
2017-06-24 20:30:41 -0600

    Firewall Modifications.

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -9,6 +9,11 @@ class profile::docker
       owner  => 'root',
       group  => 'root',
   }
+  firewall { '200 allow unifi access':
+    dport  => [80, 443, 8080, 8443, 8843, 8880, 3478, 6789, 10001],
+    proto  => tcp,
+    action =>  accept,
+  }
 
   file { '/unifi':
     ensure => directory,
