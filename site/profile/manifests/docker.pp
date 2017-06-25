a92bf61afd25248707831df748e3a1f76f479a3e
Chris Roberson <chris.roberson@chris.roberson-C02Q50FTFVH8>
2017-06-24 19:29:08 -0600

    Added docker run for unifi.

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -6,4 +6,13 @@ class profile::docker
   docker::image { 'jacobalberty/unifi':
     ensure    => 'present',
   }
+  docker::run { 'unifi':
+    image            => 'jacobalberty/unifi',
+    ports            => ['3478', '6789', 8080, 8443, 8880, 8843],
+    expose           => ['3478', '6789', 8080, 8443, 8880, 8843],
+    volumes          => ['/var/lib/couchdb', '/var/log'],
+    volumes_to       => [ '/var/lib/unifi', '/var/log/unifi' ],
+    restart_service  => true,
+    extra_parameters => [ '--restart=always' ],
+  }
 }
