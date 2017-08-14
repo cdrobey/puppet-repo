1a060c8c27ab393b222c0d1dc89893aa455556df
Chris Roberson <cdroberson@gmail.com>
2017-08-14 13:41:42 -0600

    Debugging role::master

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -5,6 +5,9 @@ class profile::master::nodemanager {
     provider => puppet_gem,
   }
 
+Node_group {
+  require => Package['puppetclassify'],
+}
   node_group { 'PE Master':
     ensure               => present,
     environment          => 'production',
@@ -12,21 +15,10 @@ class profile::master::nodemanager {
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
-      'pe_repo'                                          => {},
-      'pe_repo::platform::el_6_x86_64'                   => {},
-      'pe_repo::platform::el_7_x86_64'                   => {},
-      'pe_repo::platform::ubuntu_1404_amd64'             => {},
-      'pe_repo::platform::windows_x86_64'                => {},
-      'puppet_enterprise::profile::master'               => {
-        'code_manager_auto_configure'                    => true,
-        'r10k_remote'                                    => "https://github.com/cdrobey/puppet-repo.git",
-        'r10k_private_key'                               => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa'
-      },
-      'puppet_enterprise::profile::master::mcollective'  => {},
-      'puppet_enterprise::profile::mcollective::peadmin' => {},
-      'role::master'                                     => {},
+      'role::master' => {},
     },
   }
+
   node_group { 'Prod Linux':
     ensure               => present,
     environment          => 'production',
