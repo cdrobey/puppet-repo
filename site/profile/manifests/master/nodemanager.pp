6cc9f9e484b4dbef3fb8c6f3469937c588808770
Chris Roberson <cdroberson@gmail.com>
2017-08-14 10:36:20 -0600

    Debugging role::master

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -9,21 +9,24 @@ class profile::master::nodemanager {
     ensure               => present,
     environment          => 'production',
     override_environment => false,
-    parent               => 'PE Infrastructure',
+    parent               => 'PE Master',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
-      'ldap'                                 => {},
-      'ntp'                                  => {},
-      'pe_repo'                              => {},
-      'pe_repo::platform::el_6_x86_64'       => {},
-      'pe_repo::platform::el_7_x86_64'       => {},
-      'pe_repo::platform::ubuntu_1404_amd64' => {},
-      'pe_repo::platform::windows_x86_64'    => {},
-      'role::master'                         => {},
-      'profile::vim'                         => { 'colorscheme' => 'elflord' },
+      'pe_repo'                                          => {},
+      'pe_repo::platform::el_6_x86_64'                   => {},
+      'pe_repo::platform::el_7_x86_64'                   => {},
+      'pe_repo::platform::ubuntu_1404_amd64'             => {},
+      'pe_repo::platform::windows_x86_64'                => {},
+      'puppet_enterprise::profile::master'               => {
+        'code_manager_auto_configure'                    => true,
+        'r10k_remote'                                    => "https://github.com/cdrobey/puppet-repo.git",
+        'r10k_private_key'                               => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa'
+      },
+      'puppet_enterprise::profile::master::mcollective'  => {},
+      'puppet_enterprise::profile::mcollective::peadmin' => {},
+      'role::master'                                     => {},
     },
   }
-
   node_group { 'Prod Linux':
     ensure               => present,
     environment          => 'production',
