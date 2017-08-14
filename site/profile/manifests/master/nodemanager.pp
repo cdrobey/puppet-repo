a571f2d90c0db798c93a9a4950a31ab7685eedb0
Chris Roberson <cdroberson@gmail.com>
2017-08-14 16:16:21 -0600

    Continuing Debugging.

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -16,18 +16,18 @@ class profile::master::nodemanager {
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
-      'role::master'                          => {},
-      'pe_repo'                               => {},
-      'pe_repo::platform::el_6_x86_64'        => {},
-      'pe_repo::platform::el_7_x86_64'        => {},
-      'pe_repo::platform::ubuntu_1404_amd64'  => {},
-      'pe_repo::platform::windows_x86_64'     => {},
-      'puppet_enterprise::profile::master'               => {
+      'role::master'                                      => {},
+      'pe_repo'                                           => {},
+      'pe_repo::platform::el_6_x86_64'                    => {},
+      'pe_repo::platform::el_7_x86_64'                    => {},
+      'pe_repo::platform::ubuntu_1404_amd64'              => {},
+      'pe_repo::platform::windows_x86_64'                 => {},
+      'puppet_enterprise::profile::master::mcollective'   => {},
+      'puppet_enterprise::profile::mcollective::peadmin'  => {},
+      'puppet_enterprise::profile::master'                => {
         'code_manager_auto_configure' => true,
         'r10k_remote'                 => "https://github.com/cdrobey/puppet-repo.git",
         'r10k_private_key'            => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa' },
-      'puppet_enterprise::profile::master::mcollective'  => {},
-      'puppet_enterprise::profile::mcollective::peadmin' => {},
     },
   }
 
