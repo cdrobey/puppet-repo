742d12ac78c5df494a65abb82cd34be56d7d91cb
Chris Roberson <cdroberson@gmail.com>
2017-08-14 15:00:20 -0600

    Updating role::master

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -5,6 +5,11 @@ class profile::master::nodemanager {
     provider => puppet_gem,
   }
 
+  package { 'puppetclassify':
+    ensure   => present,
+    provider => puppet_gem,
+  }
+
   node_group { 'PE Master':
     ensure               => present,
     environment          => 'production',
@@ -12,21 +17,9 @@ class profile::master::nodemanager {
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
-      'pe_repo'                                          => {},
-      'pe_repo::platform::el_6_x86_64'                   => {},
-      'pe_repo::platform::el_7_x86_64'                   => {},
-      'pe_repo::platform::ubuntu_1404_amd64'             => {},
-      'pe_repo::platform::windows_x86_64'                => {},
-      'puppet_enterprise::profile::master'               => {
-        'code_manager_auto_configure' => true,
-        'r10k_remote'                 => 'https://github.com/cdrobey/puppet-repo',
-        'r10k_private_key'            => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa' },
-      'puppet_enterprise::profile::master::mcollective'  => {},
-      'puppet_enterprise::profile::mcollective::peadmin' => {},
       'role::master'                                     => {},
-      'profile::vim'                                     => { 'colorscheme' => 'elflord' },
     },
-    }
+  }
 
   node_group { 'Prod Linux':
     ensure               => present,
@@ -34,11 +27,11 @@ class profile::master::nodemanager {
     override_environment => false,
     parent               => 'All Nodes',
     rule                 => ['and',
-    ['not', ['=', ['fact', 'name'], $::clientcert]],
-    ['=', ['fact', 'kernel'], 'linux'],
-    ],
+                            ['not', ['=', ['fact', 'name'], $::clientcert]],
+                            ['=', ['fact', 'kernel'], 'linux'],
+                            ],
     classes              => {
-    'role::linux' => {},
+      'role::linux' => {},
     },
   }
 
@@ -49,7 +42,7 @@ class profile::master::nodemanager {
     parent               => 'All Nodes',
     rule                 => ['and', ['=', ['fact', 'kernel'], 'windows']],
     classes              => {
-    'role::windows' => {},
+      'role::windows' => {},
     },
   }
-  }
+}
