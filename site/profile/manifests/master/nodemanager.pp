d9b06a2e26913f6e276e31b0668a50a979a00007
Chris Roberson <cdroberson@gmail.com>
2017-08-14 20:38:24 -0600

    Role Update.

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -1,5 +1,5 @@
-# == Class: profile::master::nodemanager
-class profile::master::nodemanager {
+class profile::master::node_manager {
+
   package { 'puppetclassify':
     ensure   => present,
     provider => puppet_gem,
@@ -9,6 +9,10 @@ class profile::master::nodemanager {
     require => Package['puppetclassify'],
   }
 
+  # GENERAL PURPOSE
+  # Node Groups ready to go out of the box. Not tied to any specific demo, but
+  # potentially useful for demonstrating the Node Manager in general.
+
   node_group { 'PE Master':
     ensure               => present,
     environment          => 'production',
@@ -16,43 +20,21 @@ class profile::master::nodemanager {
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
-      'role::master'                                      => {},
-      'pe_repo'                                           => {},
-      'pe_repo::platform::el_6_x86_64'                    => {},
-      'pe_repo::platform::el_7_x86_64'                    => {},
-      'pe_repo::platform::ubuntu_1404_amd64'              => {},
-      'pe_repo::platform::windows_x86_64'                 => {},
-      'puppet_enterprise::profile::master::mcollective'   => {},
-      'puppet_enterprise::profile::mcollective::peadmin'  => {},
-      'puppet_enterprise::profile::master'                => {
+      'ldap'                                             => {},
+      'ntp'                                              => {},
+      'pe_repo'                                          => {},
+      'pe_repo::platform::el_6_x86_64'                   => {},
+      'pe_repo::platform::el_7_x86_64'                   => {},
+      'pe_repo::platform::ubuntu_1404_amd64'             => {},
+      'pe_repo::platform::windows_x86_64'                => {},
+      'puppet_enterprise::profile::master'               => {
         'code_manager_auto_configure' => true,
         'r10k_remote'                 => "https://github.com/cdrobey/puppet-repo.git",
         'r10k_private_key'            => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa' },
-    },
-  }
-
-  node_group { 'Prod Linux':
-    ensure               => present,
-    environment          => 'production',
-    override_environment => false,
-    parent               => 'All Nodes',
-    rule                 => ['and',
-                            ['not', ['=', ['fact', 'name'], $::clientcert]],
-                            ['=', ['fact', 'kernel'], 'linux'],
-                            ],
-    classes              => {
-      'role::linux' => {},
-    },
-  }
-
-  node_group { 'Prod Windows':
-    ensure               => present,
-    environment          => 'production',
-    override_environment => false,
-    parent               => 'All Nodes',
-    rule                 => ['and', ['=', ['fact', 'kernel'], 'windows']],
-    classes              => {
-      'role::windows' => {},
+      'puppet_enterprise::profile::master::mcollective'  => {},
+      'puppet_enterprise::profile::mcollective::peadmin' => {},
+      'role::master'                                     => {},
+      'profile::vim'                                     => { 'colorscheme' => 'elflord' },
     },
   }
 }
