1e739235862e85f876f3583303948b45cb22edb3
Chris Roberson <cdroberson@gmail.com>
2017-08-16 08:11:34 -0600

    Testing.

--- site/profile/manifests/master/nodemanager.pp
+++ site/profile/manifests/master/nodemanager.pp
@@ -1,5 +1,13 @@
-# Class: profile::master::nodemanager
-class profile::master::nodemanager {
+class profile::master::node_manager {
+
+  package { 'puppetclassify':
+    ensure   => present,
+    provider => puppet_gem,
+  }
+
+  Node_group {
+    require => Package['puppetclassify'],
+  }
 
   # GENERAL PURPOSE
   # Node Groups ready to go out of the box. Not tied to any specific demo, but
@@ -12,6 +20,8 @@ class profile::master::nodemanager {
     parent               => 'PE Infrastructure',
     rule                 => ['or', ['=', 'name', $::clientcert]],
     classes              => {
+      'ldap'                                             => {},
+      'ntp'                                              => {},
       'pe_repo'                                          => {},
       'pe_repo::platform::el_6_x86_64'                   => {},
       'pe_repo::platform::el_7_x86_64'                   => {},
@@ -19,22 +29,11 @@ class profile::master::nodemanager {
       'pe_repo::platform::windows_x86_64'                => {},
       'puppet_enterprise::profile::master'               => {
         'code_manager_auto_configure' => true,
-        'r10k_remote'                 => 'https://github.com/cdrobey/puppet-repo.git',
+        'r10k_remote'                 => "git@${gitlab_server}:puppet/control-repo.git",
         'r10k_private_key'            => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa' },
       'puppet_enterprise::profile::master::mcollective'  => {},
       'puppet_enterprise::profile::mcollective::peadmin' => {},
       'role::master'                                     => {},
+      'profile::vim'                                     => { 'colorscheme' => 'elflord' },
     },
   }
-
-  node_group { 'Linux':
-    ensure               => present,
-    environment          => 'production',
-    override_environment => false,
-    parent               => 'All Servers',
-    rule                 => ['or', ['=', 'name', $::clientcert]],
-    classes              => {
-      'role::linux' => {},
-    },
-  }
-}
