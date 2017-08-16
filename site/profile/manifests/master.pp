0cef89f62d5065cd463250d7b93db9ca08260325
Chris Roberson <cdroberson@gmail.com>
2017-08-16 07:49:45 -0600

    Role Update.

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -1,5 +1,5 @@
 # == Class: profile::master
-#class profile::master {
+class profile::master {
 #  firewall { '200 allow puppet access':
 #    dport  => [8140, 443, 61613, 8142, 4433],
 #    proto  => tcp,
