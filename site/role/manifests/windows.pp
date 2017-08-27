fd15853f6198b0fb6858bd8352f836f68dda0a14
Chris Roberson <cdroberson@gmail.com>
2017-08-27 15:19:05 -0600

    Changed baseline to linux and started Windows baseline.

--- site/role/manifests/windows.pp
+++ site/role/manifests/windows.pp
@@ -1,7 +1,5 @@
 # == Class: role::windows
 class role::windows {
   include profile::windows::accounts
-  include profile::windows::dotnet
-  include profile::windows::chocolatey
-  include profile::windows::baseline
+  include profile::windows::app
 }
