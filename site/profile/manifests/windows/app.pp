8ecf112f7b9444502037577e6dcbd4815d9aeac8
Chris Roberson <cdroberson@gmail.com>
2017-08-27 19:59:49 -0600

    Separating Windows Role into separate profiles.

--- site/profile/manifests/windows/app.pp
+++ site/profile/manifests/windows/app.pp
@@ -5,14 +5,10 @@ class profile::windows::app (
   include profile::windows::app::dotnet
   include profile::windows::app::chocolately
 
-
-  notify { "Answer is: ${packages}" }
-
-
-  # ($packages) | $name, $package | {
-  #  package { $name:
-  #    ensure          => $package['ensure'],
-  #    install_options => $package['install_options']
-  #  }
+  ($packages) | $name, $package | {
+    package { $name:
+      ensure          => $package['ensure'],
+      install_options => $package['install_options']
+    }
   }
 }
