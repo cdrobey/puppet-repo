e99b9e863527c5e353d317e661845068ced0ed24
Chris Roberson <cdroberson@gmail.com>
2017-08-27 19:03:01 -0600

    Separating Windows Role into separate profiles.

--- site/profile/manifests/windows/app/chocolately.pp
+++ site/profile/manifests/windows/app/chocolately.pp
@@ -1,4 +1,10 @@
 # == Class: profile::windows::app::chocolatey
 class profile::windows::app::chocolately {
   include chocolatey
+
+  case $operatingsystem {
+    'windows': {
+      Package { provider => chocolatey, }
+    }
+  }
 }
