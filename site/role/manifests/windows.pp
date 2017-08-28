e6849e7b19c11d0d573b5606da990ee23032f0ac
Chris Roberson <cdroberson@gmail.com>
2017-08-28 16:57:17 -0600

    Separating Windows Role into separate profiles.

--- site/role/manifests/windows.pp
+++ site/role/manifests/windows.pp
@@ -2,5 +2,5 @@
 class role::windows {
   include profile::windows::accounts
   include profile::windows::app
-  #include profile::windows::remote_access
+  include profile::windows::remote_access
 }
