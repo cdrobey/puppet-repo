dfff4a3f2b0726c56dc43bd4d2fbdcd2c5f3d454
Chris Roberson <cdroberson@gmail.com>
2017-08-28 19:21:57 -0600

    Separating Windows Role into separate profiles.

--- site/profile/manifests/windows/remote_access.pp
+++ site/profile/manifests/windows/remote_access.pp
@@ -3,6 +3,6 @@ class profile::windows::remote_access {
   include local_security_policy
 
   local_security_policy { 'Allow log on through Remote Desktop Services':
-    ensure       => present,
+    ensure       => absent,
   }
 }
