e73655423840485fea0d0c73027ff6fa69807cde
Chris Roberson <cdroberson@gmail.com>
2017-08-29 08:00:56 -0600

    Adding Set Time to Windows Baseline.

--- site/profile/manifests/windows/time.pp
+++ site/profile/manifests/windows/time.pp
@@ -6,4 +6,10 @@ class profile::windows::time (
   class { 'winntp':
     servers => $ntp_servers,
   }
+
+  dsc_xtimezone { 'set timezone':
+    dsc_timezone         => 'Central Standard Time',
+    dsc_issingleinstance => 'yes',
+    require              => Package['powershell'],
+  }
 }
