86b15934da698e385565d313ad918e136b83a31b
Chris Roberson <cdroberson@gmail.com>
2017-09-04 15:13:03 -0600

    Fixed Powershell Declaration Error.

--- site/profile/manifests/windows/rdp.pp
+++ site/profile/manifests/windows/rdp.pp
@@ -1,10 +1,9 @@
 # == Class: profile::windows::rdp
 class profile::windows::rdp (
   $enable,
-  $nla_enable,
   ){
   class { 'rdp':
     rdp_enable     => $enable,
-    rdp_nla_enable => $nla_enable,
+    rdp_nla_enable => true,
   }
 }
