615a2d104d75426fd49e32c66b5fab2a6703ebf1
Chris Roberson <cdroberson@gmail.com>
2017-09-03 23:22:26 -0600

    Created Printing Profile and Role.

--- site/profile/manifests/print.pp
+++ site/profile/manifests/print.pp
@@ -1,9 +1,14 @@
 # == Class: profile::print
 class profile::print {
 
-  firewall { '200 allow unifi access':
-    dport  => [80, 443, 631, 515],
+  firewall { '200 allow tcp printer access':
+    dport  => [80, 443, 631, 515, 5353],
     proto  => tcp,
+    action =>  accept,
+  }
+  firewall { '200 allow udp printer access':
+    dport  => [631, 515, 5353],
+    proto  => udp,
     action =>  accept,
   }
 
