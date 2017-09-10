4f56f6b4b7959c727438c7d58928cb1b02dc22c0
Chris Roberson <cdroberson@gmail.com>
2017-09-10 16:17:46 -0600

    Add release parameter to unifi.

--- site/profile/manifests/unifi.pp
+++ site/profile/manifests/unifi.pp
@@ -1,12 +1,15 @@
 # == Class: profile::docker
-class profile::unifi
-{
+class profile::unifi (
+  $repo_release,
+){
   firewall { '200 allow unifi access':
     dport  => [80, 443, 8080, 8443, 8843, 8880, 3478, 6789, 10001],
     proto  => tcp,
     action =>  accept,
   }
 
-  class { 'unifi': }
+  class { 'unifi':
+    repo_release => $repo_release,
+  }
 
 }
