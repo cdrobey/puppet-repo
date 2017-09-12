e884f9ece7ad3ca4ea16648bf3ba0b748b55dead
Chris Roberson <cdroberson@gmail.com>
2017-09-12 12:26:25 -0600

    Modified Windows accounts.pp to always load admin account and purge all other non-puppetized accounts.

--- site/profile/manifests/windows/accounts.pp
+++ site/profile/manifests/windows/accounts.pp
@@ -2,6 +2,25 @@
 class profile::windows::accounts (
   $users,
 ) {
+  # Permanent Account for Administrative Access
+  user { 'admin':
+    ensure     => present,
+    groups     => ['Administrators'],
+    comment    => 'Local Administrators',
+    managehome => yes,
+    password   => 'T3st!Passw0rd',
+  }
+
+  # Purge all local accounts except permanent administrative accounts.
+  purge { 'user':
+    unless      => [
+      [ 'name', '==', 'Administrator' ],
+      [ 'name', '==', 'admin' ],
+      [ 'name', '==', 'guest' ],
+    ]
+  }
+
+  # Add all dynamic accounts
   each ($users) | $name, $user | {
     user { $name:
       ensure     => $user['ensure'],
