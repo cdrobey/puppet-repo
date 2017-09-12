caf9db7f7a37a32c4506cea03719fe5c0f1bb946
Chris Roberson <cdroberson@gmail.com>
2017-09-11 21:33:53 -0600

    Updated rdc.

--- site/profile/manifests/windows/time.pp
+++ site/profile/manifests/windows/time.pp
@@ -5,15 +5,18 @@ class profile::windows::time (
 ) {
 
 
-  # Module relies on puppet-powershelldsc for settingthe time zone.
-  # You must enable winrm and execution policy.
+  class { 'winntp':
+    servers => $ntp_servers,
+  }
+
   registry_value { 'HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell\ExecutionPolicy':
     ensure => present,
     data   => 'Unrestricted',
   }
 
   dsc_xtimezone { 'set timezone':
-    dsc_timezone         => 'Mountain Standard Time',
+  #dsc_timezone         => 'Mountain Standard Time',
+    dsc_timezone         => $timezone,
     dsc_issingleinstance => 'yes',
     require              => Package['powershell'],
   }
