b4a09d7fcb5cd27655a24b13c88ee611af81615e
Chris Roberson <cdroberson@gmail.com>
2017-09-11 22:02:59 -0600

    Updated rdc.

--- site/profile/manifests/windows/time.pp
+++ site/profile/manifests/windows/time.pp
@@ -3,8 +3,6 @@ class profile::windows::time (
   $timezone,
   $ntp_servers,
 ) {
-
-
   class { 'winntp':
     servers => $ntp_servers,
   }
@@ -14,8 +12,18 @@ class profile::windows::time (
     data   => 'Unrestricted',
   }
 
+  reboot { 'time_reboot':
+    when => 'pending',
+  }
+
+  dsc_systemlocale { 'set timezone':
+    dsc_systemlocale     => 'en-us',
+    dsc_issingleinstance => 'yes',
+    require              => Package['powershell'],
+    notify               => Reboot['time_reboot'],
+  }
+
   dsc_xtimezone { 'set timezone':
-  #dsc_timezone         => 'Mountain Standard Time',
     dsc_timezone         => $timezone,
     dsc_issingleinstance => 'yes',
     require              => Package['powershell'],
