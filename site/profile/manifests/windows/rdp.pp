e2cab93f969b900ef20b26d06fd72e6a1e8c5342
Chris Roberson <cdroberson@gmail.com>
2017-09-15 06:33:48 -0600

    Migrated RDP from module to profile.  Removed firewall requirement.

--- site/profile/manifests/windows/rdp.pp
+++ site/profile/manifests/windows/rdp.pp
@@ -3,10 +3,39 @@ class profile::windows::rdp (
   $rdp_enable,
   $rdp_nla_enable,
   ){
-  class { 'rdp':
-    rdp_enable     => $rdp_enable,
-    rdp_nla_enable => $rdp_nla_enable,
+  
+  # Remote Desktop Connection logic (Remote Desktop is enabled = 0, Remote Desktop is disabled = 1)
+  if $rdp_enable {
+    $rdp_setting = '0'
   }
+  else {
+    $rdp_setting = '1'
+  }
+
+  # Remote Desktop Network Level Authentication logic (Remote Desktop NLA is enabled = 1, Remote Desktop NLA is disabled = 0)
+  if $rdp_nla_enable {
+    $rdp_nla_setting = '1'
+  }
+  else {
+    $rdp_nla_setting = '0'
+  }
+
+  # Remote Desktop Connection
+  registry::value { 'rdp':
+    key   => 'HKLM\System\CurrentControlSet\Control\Terminal Server',
+    value => 'fDenyTSConnections',
+    type  => 'dword',
+    data  => $rdp_setting,
+  }
+
+  # Remote Desktop Network Level Authentication
+  registry::value { 'rdp_nla':
+    key   => 'HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp',
+    value => 'UserAuthentication',
+    type  => 'dword',
+    data  => $rdp_nla_setting,
+  }
+  
   # Set time limit for active but idle Remote Desktop Services sessions: 10 minutes
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Terminal Services\MaxIdleTime':
     type   => dword,
