768a8400214fe2686fa98732290acbd54037363b
Chris Roberson <cdroberson@gmail.com>
2017-09-15 06:26:37 -0600

    Cleaned Puppet Lint Errors.

--- site/profile/manifests/windows/rdp.pp
+++ site/profile/manifests/windows/rdp.pp
@@ -7,7 +7,6 @@ class profile::windows::rdp (
     rdp_enable     => $rdp_enable,
     rdp_nla_enable => $rdp_nla_enable,
   }
-  
   # Set time limit for active but idle Remote Desktop Services sessions: 10 minutes
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Terminal Services\MaxIdleTime':
     type   => dword,
