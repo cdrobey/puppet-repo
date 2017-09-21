7c03c8f12a1fddbef172f0bd9f3c2ea34ed71e33
Chris Roberson <cdroberson@gmail.com>
2017-09-21 09:55:44 -0600

    Setup class parameter for start.

--- site/profile/manifests/apprc.pp
+++ site/profile/manifests/apprc.pp
@@ -3,5 +3,6 @@ class profile::apprc {
     class { 'apprc':
         service_name     => 'testsvc',
         service_validate => 'testvalidate',
+        server_start     => 'Overriding the starting value.',
     }
 }
