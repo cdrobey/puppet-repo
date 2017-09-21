cdbf142f308338b1b31ddb78e7023eac4e7b71bd
Chris Roberson <cdroberson@gmail.com>
2017-09-20 22:54:03 -0600

    Added start and stop to the profile.

--- site/profile/manifests/apprc.pp
+++ site/profile/manifests/apprc.pp
@@ -3,5 +3,7 @@ class profile::apprc {
     class { 'apprc':
         service_name     => 'testsvc',
         service_validate => 'testvalidate',
+        service_start    => 'echo this is a start test.'
+        service_stop     => 'echo this is a stop test.'
     }
 }
