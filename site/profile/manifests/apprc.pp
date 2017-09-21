9eeed1d8474d69f90b21de240a4442c482870124
Chris Roberson <cdroberson@gmail.com>
2017-09-21 10:12:57 -0600

    Starting to use hiera for management.

--- site/profile/manifests/apprc.pp
+++ site/profile/manifests/apprc.pp
@@ -1,14 +1,14 @@
 # == Class: profile::apprc
 class profile::apprc (
-    service_name,
-    service_validate,
-    service_start,
-    service_stop,
+    app_name,
+    app_validate,
+    app_start,
+    app_stop,
 ){
     class { 'apprc':
         service_name     => $app_name,
         service_validate => $app_validate,
         service_start    => $app_start,
-        service_start    => $app_stop,
+        service_stop     => $app_stop,
     }
 }
