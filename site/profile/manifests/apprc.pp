bff68198a2faef32ba6a33cd8969086791a3e6c1
Chris Roberson <cdroberson@gmail.com>
2017-09-21 10:45:40 -0600

    Fixing Lint Errors.

--- site/profile/manifests/apprc.pp
+++ site/profile/manifests/apprc.pp
@@ -7,10 +7,10 @@ class profile::apprc (
     $app_proc,
 ){
     class { 'apprc':
-        service_name     => $app_name,
-        service_validate => $app_validate,
-        service_start    => $app_start,
-        service_stop     => $app_stop,
-        service_proc     => $app_proc,
+        app_name     => $app_name,
+        app_validate => $app_validate,
+        app_start    => $app_start,
+        app_stop     => $app_stop,
+        app_proc     => $app_proc,
     }
 }
