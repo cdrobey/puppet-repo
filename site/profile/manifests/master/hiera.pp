8f192f696c74cbb1f6fe8f7cb1115802f3e900be
Chris Roberson <cdroberson@gmail.com>
2017-09-22 14:28:04 -0600

    Upgrading to Hiera 5.

--- site/profile/manifests/master/hiera.pp
+++ site/profile/manifests/master/hiera.pp
@@ -1,17 +1,12 @@
 # == Class: profile::master::hiera
 class profile::master::hiera {
-  class { 'hiera':
-    datadir_manage => false,
-    datadir        => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
-    eyaml          => true,
-    hierarchy      => [
-      'nodes/%{clientcert}',
-      'environment/%{environment}',
-      'common',
-    ],
-  }
-
-  Service <| title == 'pe-puppetserver' |> {
-    subscribe => Class['hiera'],
-  }
+ class { 'hiera':
+#     hiera_version   =>  '5',
+#     hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
+#     hierarchy       =>  [
+#                                 {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+#                                 {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+#                                 {"name" =>  "Global yaml file", "path" =>  "common.yaml"},
+#                         ],
+#   }
 }
