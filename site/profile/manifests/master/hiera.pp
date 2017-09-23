0f26246ecb02602da166c42c8d20f5bd3379e16e
Chris Roberson <cdroberson@gmail.com>
2017-09-23 13:42:31 -0600

    Code Cleanup.

--- site/profile/manifests/master/hiera.pp
+++ site/profile/manifests/master/hiera.pp
@@ -1,15 +1,15 @@
 # == Class: profile::master::hiera
 class profile::master::hiera {
-  class { 'hiera':
-          hiera_version   =>  '5',
-          hiera5_defaults =>  {"datadir" => "/etc/puppetlabs/code/environments/%{::environment}/hieradata", "data_hash" => "yaml_data"},
-          hierarchy       =>  [
-                                {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
-                                {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
-                                {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
-                              ],
-          eyaml           => true,
-          eyaml_node      => hiera-eyaml,
+  class { 'hiera': 
+    hiera_version   =>  '5',
+    hiera5_defaults =>  {"datadir" => "/etc/puppetlabs/code/environments/%{::environment}/hieradata", "data_hash" => "yaml_data"},
+    hierarchy       =>  [ 
+      {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+      {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+      {"name" =>  "Default yaml file", "path" =>  "common.yaml"}, 
+    ],
+    eyaml           => true,
+    eyaml_name      => hiera-eyaml,
   }
 
 }
