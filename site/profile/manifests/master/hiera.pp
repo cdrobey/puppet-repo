ad5eeec9eba6b2d111fa8f4b77cb684d5dbc6fa3
Chris Roberson <cdroberson@gmail.com>
2017-09-23 13:15:33 -0600

    Using full path for datadir.

--- site/profile/manifests/master/hiera.pp
+++ site/profile/manifests/master/hiera.pp
@@ -2,7 +2,7 @@
 class profile::master::hiera {
   class { 'hiera':
           hiera_version   =>  '5',
-          hiera5_defaults =>  {"datadir" => "hieradata", "data_hash" => "yaml_data"},
+          hiera5_defaults =>  {"datadir" => "/etc/puppetlabs/code/environments/%{::environment}/hieradata", "data_hash" => "yaml_data"},
           hierarchy       =>  [
                                 {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
                                 {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
