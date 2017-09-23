d9bc8f521c13def31d483c147ec2ded04be8e7bf
Chris Roberson <cdroberson@gmail.com>
2017-09-23 12:27:36 -0600

    Changed hiera data directory to hieradata.

--- site/profile/manifests/master/hiera.pp
+++ site/profile/manifests/master/hiera.pp
@@ -2,11 +2,11 @@
 class profile::master::hiera {
   class { 'hiera':
           hiera_version   =>  '5',
-          hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
+          hiera5_defaults =>  {"datadir" => "hieradata", "data_hash" => "yaml_data"},
           hierarchy       =>  [
                                 {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
                                 {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
                                 {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
                               ],
-}
+  }
 }
