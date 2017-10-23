25b73a05b57032a725f35da507dd1e63d87b6d1d
Chris Roberson <cdroberson@gmail.com>
2017-10-23 13:49:40 -0600

    Update master firewall.

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -1,8 +1,8 @@
 # == Class: profile::master
 class profile::master
 {
-  firewall { '200 allow unifi access':
-    dport  => [80, 443, 8140],
+  firewall { '200 allow puppet access':
+    dport  => [80, 443, 8140, 8142, 61613],
     proto  => tcp,
     action =>  accept,
   }
