fc77d089b979a9f08f0d35cc7f69013fd8d3ff31
Chris Roberson <cdroberson@gmail.com>
2017-10-25 16:05:46 -0600

    Configured all Puppet Enterprise Ports in the Firewall.

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -2,7 +2,7 @@
 class profile::master
 {
   firewall { '200 allow puppet access':
-    dport  => [80, 443, 8140, 8142, 8143, 8170, 61613],
+    dport  => [80, 443, 4433, 8081, 8140, 8142, 8143, 8170, 61613],
     proto  => tcp,
     action =>  accept,
   }
