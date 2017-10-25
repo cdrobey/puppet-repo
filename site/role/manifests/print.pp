3c4e829b3647d1b18740c18f0bc676fe3f74cf4b
Chris Roberson <cdroberson@gmail.com>
2017-10-25 12:23:31 -0600

    Updated Comment Headers on all roles.

--- site/role/manifests/print.pp
+++ site/role/manifests/print.pp
@@ -1,3 +1,15 @@
+# apprc
+#
+# Install and configure rc script for the defined application with startup validation.
+#
+# @summary  This class creates a mechanism to manage rc scripts for applications on RHEL/Centos7
+#           system.  The script includes the capability to add a post-exec test validationn script
+#           to ensure the health of the application after startup.
+#
+# @param apps [Hash] Hash table that maps an application, validate, and start and stop procedures.
+#
+# @example
+#   include apprc
 # == Class: role::print
 class role::print {
   include profile::linux
