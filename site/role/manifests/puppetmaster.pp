78d08add40b09cf9c667653eca35963536ec7e92
Chris Roberson <cdroberson@gmail.com>
2017-12-22 08:46:58 -0700

    Changed from master role to puppetmaster role definition.

---
+++ site/role/manifests/puppetmaster.pp
@@ -0,0 +1,17 @@
+# puppetmaster
+#
+# Install and configure PE puppetmaster server
+#
+# @summary  This role delivers a PE puppetmaster server to perform management across a set of puppet managed clients.
+#           The role applies the baseline configuration to the puppet puppetmaster and opens all ports needed for a
+#           monolithic Puppet install.
+#
+# @param    none
+#
+# @example
+#   include role::puppetmaster or assign in PE classifier
+# == Class: role::puppetmaster
+class role::puppetmaster {
+  include 'profile::base'
+  include 'profile::apps::puppetmaster'
+}
