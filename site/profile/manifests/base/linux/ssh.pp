f9b5445dc84e4f40da2f22d7f54113767132d82d
Chris Roberson <cdroberson@gmail.com>
2018-01-23 11:50:58 -0700

    Deleted forced firewall configuration.

--- site/profile/manifests/base/linux/ssh.pp
+++ site/profile/manifests/base/linux/ssh.pp
@@ -30,6 +30,5 @@ class profile::base::linux::ssh (
     sshd_password_authentication => $password_authentication,
     permit_root_login            => $permit_root_login,
     sshd_config_banner           => $banner,
-    manage_firewall              => false,
   }
 }
