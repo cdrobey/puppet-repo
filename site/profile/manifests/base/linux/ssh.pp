1d06b1c3d2dc191f690248ac75145537011ab61c
Chris Roberson <cdroberson@gmail.com>
2018-01-23 11:39:55 -0700

    Enabled Firewall management.

--- site/profile/manifests/base/linux/ssh.pp
+++ site/profile/manifests/base/linux/ssh.pp
@@ -24,8 +24,7 @@ class profile::base::linux::ssh (
   class { 'ssh':
     sshd_password_authentication => $password_authentication,
     permit_root_login            => $permit_root_login,
-  #  sshd_config_permitemptypasswords => $permit_empty_passwords,
-  #  manage_firewall                  => $manage_firewall,
+    manage_firewall              => $manage_firewall,
     sshd_config_banner           => $banner,
   }
 }
