13906b1db054b7d051daaf167442eb3a0133e37a
Chris Roberson <cdroberson@gmail.com>
2018-01-23 11:35:20 -0700

    debugging ssh.

--- site/profile/manifests/base/linux/ssh.pp
+++ site/profile/manifests/base/linux/ssh.pp
@@ -21,12 +21,11 @@ class profile::base::linux::ssh (
   $manage_firewall,
   $banner,
 ) {
-  include ssh
-  #class { 'ssh':
-  #  sshd_password_authentication     => $password_authentication,
+  class { 'ssh':
+    sshd_password_authentication     => $password_authentication,
   #  permit_root_login                => $permit_root_login,
   #  sshd_config_permitemptypasswords => $permit_empty_passwords,
   #  manage_firewall                  => $manage_firewall,
   #  sshd_config_banner               => $banner,
-  #}
+  }
 }
