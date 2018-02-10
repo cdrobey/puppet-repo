16817c99eedec508fcf1a373ae43140125be1929
Chris Roberson <cdroberson@gmail.com>
2018-02-10 10:41:45 -0700

    Debugging.

--- site/profile/manifests/os/monitor.pp
+++ site/profile/manifests/os/monitor.pp
@@ -28,11 +28,11 @@ class profile::os::monitor (
       },
       inputs   => $influxdbinputs,
     }
+    notice ("influxdbd = ${influxdbd}")
     $influxdbd.each | $influxdbdname, $influxdbdinputs | {
-#      telegraf::input { $influxdbdname:
-#        plugin_type => $influxdbd['plugin_type'],
-#        options     => $influxdbd['options'],
-        notice("${influxdbdname['plugin_type']} = ${influxdbdname['options']}"
+      telegraf::input { $influxdbdname:
+        plugin_type => $influxdbd['plugin_type'],
+        options     => $influxdbd['options'],
       }
     }
   }
