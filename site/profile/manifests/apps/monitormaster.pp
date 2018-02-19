3a033d05b70dcfe97ba507c4a29d4a8835a1afb8
Chris Roberson <cdroberson@gmail.com>
2018-02-18 21:35:36 -0700

    Removed user for influxdb.

--- site/profile/manifests/apps/monitormaster.pp
+++ site/profile/manifests/apps/monitormaster.pp
@@ -29,13 +29,11 @@ class profile::apps::monitormaster (
     -> class { 'grafana':
     }
     grafana_datasource { 'influxdb':
-        grafana_url      => 'http://localhost:3000',
-        grafana_user     => 'admin',
-        grafana_password => 'admin',
-        type             => 'influxdb',
-        url              => 'http://localhost:8086',
-        database         => 'Monitor',
-        access_mode      => 'proxy',
-        is_default       => true,
+        grafana_url => 'http://localhost:3000',
+        type        => 'influxdb',
+        url         => 'http://localhost:8086',
+        database    => 'Monitor',
+        access_mode => 'proxy',
+        is_default  => true,
     }
 }
