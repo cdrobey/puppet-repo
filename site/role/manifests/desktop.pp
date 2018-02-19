c7ab809682e37ca1f4ca90853fe655531f78a0e2
Chris Roberson <cdroberson@gmail.com>
2018-02-19 09:20:58 -0700

    Setup desktop role.

---
+++ site/role/manifests/desktop.pp
@@ -0,0 +1,16 @@
+# desktop
+#
+# Install and configure a desktop OS.
+#
+# @summary  This role delivers a desktop baseline configuration for a minimal secure linux and windows
+#           configurations. It integrates a series of profiles built for baseline system compliance.
+#
+# @param    none
+#
+# @example
+#   include role::desktop or assign in PE classifier
+# == Class: role::desktop
+class role::desktop {
+  include 'profile::base'
+  include profile::os::monitor
+}
