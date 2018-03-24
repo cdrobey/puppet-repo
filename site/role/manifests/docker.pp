4362a17d68a35f214fc9b8d40a2b5a46515ecf75
Chris Roberson <chris.roberson@puppet.com>
2018-03-23 22:32:16 -0600

    Create docker role.

---
+++ site/role/manifests/docker.pp
@@ -0,0 +1,15 @@
+# docker
+#
+# Install and configure canary docker profile.
+#
+# @summary  This role delivers a docker configuration for checking different profiles.
+#
+# @param    none
+#
+# @example
+#   include role::docker or assign in PE classifier
+# == Class: role::docker
+class role::docker {
+  include profile::base
+  include profile::os::docker
+}
