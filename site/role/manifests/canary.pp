016cfb34fb32023a679a016dcddadfdee903e1ef
Chris Roberson <chris.roberson@puppet.com>
2018-04-01 18:34:40 -0600

    Setup canary profile.

---
+++ site/role/manifests/canary.pp
@@ -0,0 +1,15 @@
+# canary
+#
+# Install and configure canary system profiles for testting.
+#
+# @summary  This role delivers a canary configuration for checking different profiles in a development
+#   environment.
+#
+# @param    none
+#
+# @example
+#   include role::canary or assign in PE classifier
+# == Class: role::canary
+class role::canary {
+  include profile::base
+}
