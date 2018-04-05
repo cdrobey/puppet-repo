b58dd2b361b259bb8cd4288137e0dccf91c76ce7
Chris Roberson <chris.roberson@puppet.com>
2018-04-04 18:07:43 -0600

    Breakup mgmt role into independent roles.

---
+++ site/role/manifests/monitormaster.pp
@@ -0,0 +1,17 @@
+# monitormaster
+#
+# Install and configure monitor management  server
+#
+# @summary  This role delivers a monitor application server to monitor systems services.  The role
+#           builds a monitor station based upon grafana/influxdb/telegraf.
+#
+# @param    none
+#
+# @example
+#   include role::monitormaster or assign in PE classifier
+# == Class: role::monitormaster
+class role::monitormaster {
+  include profile::base
+  include profile::os::monitor
+  include profile::apps::monitormaster
+}
