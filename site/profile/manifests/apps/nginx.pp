a4d5a8e2b713614bf2b2b587ba7f5181b791f3bd
Chris Roberson <chris.roberson@puppet.com>
2018-04-14 18:22:03 -0600

    Created directory.

--- site/profile/manifests/apps/nginx.pp
+++ site/profile/manifests/apps/nginx.pp
@@ -29,7 +29,7 @@ class profile::apps::nginx (
   letsencrypt::certonly { 'letsencryptcert':
     domains              => $certdomains,
     plugin               => 'webroot',
-    webroot_paths        => ['/etc/nginx/ssl'],
+    webroot_paths        => ['/etc/letsencrypt'],
     manage_cron          => true,
     suppress_cron_output => true,
     before               => Class['nginx'],
