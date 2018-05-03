e342a70a23d3e88f464fb9bd4fe1b50f0411900e
Chris Roberson <chris.roberson@puppet.com>
2018-05-03 07:32:34 -0600

    Applied Correct Resource Value.

--- site/profile/manifests/os/daas.pp
+++ site/profile/manifests/os/daas.pp
@@ -20,15 +20,22 @@ class profile::os::daas (
     source => 'https://s3.amazonaws.com/jumpcloud-windows-agent/production/JumpCloudInstaller.exe',
   }
 
-  -> dsc_package { 'JumpCloudInstaller':
-    dsc_ensure    => 'present',
-    dsc_name      => 'JumpCloud',
-    dsc_productid => '96542816-DAD1-4D02-8363-CA4121E5CAE7',
-    dsc_path      => 'C:\\JumpCloudInstaller.exe',
-    dsc_arguments => '-k 5ae9d5a0f9f52a0e29ad3a28 /VERYSILENT /NORESTART',
+  #-> dsc_package { 'JumpCloud v1.0':
+    #dsc_ensure    => 'present',
+    #dsc_name      => 'JumpCloud v1.0',
+    #dsc_productid => '96542816-DAD1-4D02-8363-CA4121E5CAE7',
+    #dsc_path      => 'C:\\JumpCloudInstaller.exe',
+  # dsc_arguments => '-k 5ae9d5a0f9f52a0e29ad3a28 /VERYSILENT /NORESTART',
+  #}
+
+
+  -> package { 'JumpCloud v1.0':
+    ensure          => installed,
+    source          => 'C:\\JumpCloudInstaller.exe',
+    install_options => ['-k 5ae9d5a0f9f52a0e29ad3a28', '/VERYSILENT', '/NORESTART']
   }
 
-  -> service { 'JumpCloudInstaller':
+  -> service { 'JumpCloud v1.0':
     ensure => 'running',
     enable => true,
   }
