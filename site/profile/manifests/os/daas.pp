96ec4148d9a2a0ee3ccaa843c1b325f2a59d548b
Chris Roberson <chris.roberson@puppet.com>
2018-05-03 07:54:57 -0600

    Use url install path.

--- site/profile/manifests/os/daas.pp
+++ site/profile/manifests/os/daas.pp
@@ -12,22 +12,12 @@
 #   include profile::os::daas or assign in PE classifier
 # == Class: profile::os::daas
 class profile::os::daas (
-#  $installurl,
-#  $systemuuid,
+  String $installurl = 'https://s3.amazonaws.com/jumpcloud-windows-agent/production/JumpCloudInstaller.exe',
 ){
   remote_file { 'C:\\JumpCloudInstaller.exe':
     ensure => present,
-    source => 'https://s3.amazonaws.com/jumpcloud-windows-agent/production/JumpCloudInstaller.exe',
+    source => $installurl,
   }
-
-  #-> dsc_package { 'JumpCloud v1.0':
-    #dsc_ensure    => 'present',
-    #dsc_name      => 'JumpCloud v1.0',
-    #dsc_productid => '96542816-DAD1-4D02-8363-CA4121E5CAE7',
-    #dsc_path      => 'C:\\JumpCloudInstaller.exe',
-  # dsc_arguments => '-k 5ae9d5a0f9f52a0e29ad3a28 /VERYSILENT /NORESTART',
-  #}
-
 
   -> package { 'JumpCloud v1.0':
     ensure          => '1.0',
@@ -35,8 +25,7 @@ class profile::os::daas (
     install_options => ['-k 5ae9d5a0f9f52a0e29ad3a28', '/VERYSILENT', '/NORESTART']
   }
 
-# -> service { 'JumpCloud v1.0':
-#   ensure => 'running',
-#   enable => true,
-# }
-}
+  -> service { 'jumpcloud-agent':
+    ensure => 'running',
+    enable => true,
+  }
