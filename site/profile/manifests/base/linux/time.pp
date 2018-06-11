23fe777e9e0c67a2441b63956bae73c341b318d4
Chris Roberson <chris.roberson@puppet.com>
2018-06-10 22:25:39 -0600

    Update time.

--- site/profile/manifests/base/linux/time.pp
+++ site/profile/manifests/base/linux/time.pp
@@ -12,7 +12,7 @@
 #   include profile::base::linux::time or assign in PE classifier
 # == Class: profile::base::linux::time
 class profile::base::linux::time (
-  $timezone,
+  String $timezone,
   Array $ntp_servers,
 ) {
 
