705fc2b7dbf070bd1aeda565b53e41bf92095318
Chris Roberson <chris.roberson@puppet.com>
2018-10-28 22:25:04 -0600

    Update version of ELK.

--- site/profile/manifests/apps/graylog.pp
+++ site/profile/manifests/apps/graylog.pp
@@ -22,7 +22,7 @@ class profile::apps::graylog (
     distribution => 'jre',
   }
   -> class { 'elasticsearch':
-    version     => '5',
+    version     => '6.4.2',
     manage_repo => true,
   }
   -> elasticsearch::instance { 'graylog':
