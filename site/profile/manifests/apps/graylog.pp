e9f1599cbc43ca74aa85baddd9800283a32fbb4e
Chris Roberson <chris.roberson@puppet.com>
2018-11-06 08:15:31 -0700

    Add mongo and elastic URL.

--- site/profile/manifests/apps/graylog.pp
+++ site/profile/manifests/apps/graylog.pp
@@ -12,11 +12,20 @@
 # == Class: profile::apps::graylog
 class profile::apps::graylog (
 ){
-
   firewall { '300 allow graylog access':
-      dport  => [9000],
-      proto  => tcp,
-      action =>  accept,
+    dport  => [9000],
+    proto  => tcp,
+    action =>  accept,
+  }
+  firewall { '301 allow elasticsearch access':
+    dport  => [9200],
+    proto  => tcp,
+    action =>  accept,
+  }
+  firewall { '300 allow graylog access':
+    dport  => [9000],
+    proto  => tcp,
+    action =>  accept,
   }
 
   class {'mongodb::globals':
@@ -44,11 +53,15 @@ class profile::apps::graylog (
     version => '2.4'
   }
   -> class { 'graylog::server':
-    config          => {
-      'password_secret'    => 'mOP51XcWO1TQ8ajtz2k=',
-      'root_password_sha2' => '69fef8305e2165a3427f19a6fc09786beaa943dee3b460e68d4e1a61c6bce25d  -',
-      'web_enable'         => true,
-      'web_listen_uri'     => 'http://co-c75-glp01.local.familyroberson.com:9000/',
+    config => {
+      'password_secret'     => 'mOP51XcWO1TQ8ajtz2k=',
+      'root_password_sha2'  => '69fef8305e2165a3427f19a6fc09786beaa943dee3b460e68d4e1a61c6bce25d  -',
+      'web_enable'          => true,
+      'web_listen_uri'      => "http://${facts['fqdn']}:9000",
+      'rest_listen_uri'     => "http://${facts['fqdn']}:9000/api/",
+      'rest_transport_uri'  => "http://${facts['fqdn']}:9000/api/",
+      'elasticsearch_hosts' => "http://${facts['fqdn']}:9200",
+      'mongodb_uri'         => "mongodb://${facts['fqdn']}:27017",
     }
   }
 }
