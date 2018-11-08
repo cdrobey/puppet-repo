ea1a6580c0be361702f98ad50a2e2ce78f2f7f5c
Chris Roberson <chris.roberson@puppet.com>
2018-11-07 19:35:11 -0700

    Update localhost to fqdn.

--- site/profile/manifests/apps/graylog.pp
+++ site/profile/manifests/apps/graylog.pp
@@ -56,7 +56,7 @@ class profile::apps::graylog (
   elasticsearch::instance { 'graylog':
     config => {
       'cluster.name' => 'graylog',
-      'network.host' => '127.0.0.1',
+      'network.host' => $facts['fqdn'],
     }
   }
 
