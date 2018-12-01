57165e9b3c5086f7b9ae88ffe074cd9ccda25a6e
Chris Roberson <chris.roberson@puppet.com>
2018-12-01 00:20:19 -0700

    Add Hash default.

--- site/profile/manifests/apps/samba.pp
+++ site/profile/manifests/apps/samba.pp
@@ -12,8 +12,8 @@
 # == Class: profile::apps::samba
 class profile::apps::samba (
   String $workgroup = 'HOME',
-  Hash $users = undef,
-  Hash $shares = undef,
+  Hash $users = [],
+  Hash $shares = [],
 ){
 
   firewall { '300 allow tcp communication to smbd/nbmd':
