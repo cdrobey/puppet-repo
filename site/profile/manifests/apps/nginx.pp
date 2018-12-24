3ca23e864062683bd4d7c62171aae43fc0731bec
Chris Roberson <chris.roberson@puppet.com>
2018-12-23 22:34:41 -0700

    Set empy arry.

--- site/profile/manifests/apps/nginx.pp
+++ site/profile/manifests/apps/nginx.pp
@@ -28,7 +28,7 @@ class profile::apps::nginx (
   nginx::resource::server { 'co-c75-nginxp01.local.familyroberson.com':
     use_default_location => false,
     server_cfg_append    => { 'return 301' => 'https://$server_name$request_uri' },
-    index_files          => [],
+    index_files          => [''],
   }
 
   $proxylist.each | $proxy_name, $proxy | {
