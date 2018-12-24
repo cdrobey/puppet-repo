ea5d2a9e15d9e7df4e15dbede86b9be4dc056d7f
Chris Roberson <chris.roberson@puppet.com>
2018-12-23 21:14:38 -0700

    Simplify Facts.

--- site/profile/manifests/apps/nginx.pp
+++ site/profile/manifests/apps/nginx.pp
@@ -25,7 +25,7 @@ class profile::apps::nginx (
 
   class { 'nginx': }
 
-  nginx::resource::server { "$facts['hostname'].$facts['fqdq']":
+  nginx::resource::server { $facts['hostname']:
     ensure              => present,
     www_root            => '/var/www',
     location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
