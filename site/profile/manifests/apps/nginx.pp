63dd309e230e87ab269c7730efdecb36002120f7
Chris Roberson <chris.roberson@puppet.com>
2018-12-24 11:29:34 -0700

    Cleanup and rewrite to servername.

--- site/profile/manifests/apps/nginx.pp
+++ site/profile/manifests/apps/nginx.pp
@@ -12,8 +12,6 @@
 # == Class: profile::apps::nginx
 # test comment
 class profile::apps::nginx (
-  Array $serverlist,
-  Integer $proxyport,
   Hash $proxylist,
   Array $proxysetheaders,
 ){
@@ -26,20 +24,13 @@ class profile::apps::nginx (
 
   class { 'nginx': }
 
-  #nginx::resource::server { 'default':
-  #  use_default_location => false,
-  #  server_name          => ['*.familyroberson.com'],
-  #  index_files          => [],
-  # server_cfg_append    => { 'return' => '301 https://*.familyroberson.com?request_uri' },
-  #
-
   $proxylist.each | $proxy_name, $proxy | {
     nginx::resource::server { "${proxy_name}-80":
       server_name      => [ $proxy_name ],
       listen_port          => 80,
       use_default_location => false,
       index_files          => [],
-      server_cfg_append    => { 'return' => '301 https://$host$request_uri' },
+      server_cfg_append    => { 'return' => '301 https://$server_name$request_uri' },
     }
 
     nginx::resource::server{ "${proxy_name}-443":
