a686a3226b77292fe0cf6444b417e6a360d0f67a
Chris Roberson <chris.roberson@puppet.com>
2019-02-02 15:49:02 -0700

    Add plex as container.

--- site/profile/manifests/os/docker.pp
+++ site/profile/manifests/os/docker.pp
@@ -54,4 +54,32 @@ class profile::os::docker (
     pull_on_start   => false,
     docker_service  => true,
   }
+
+  docker_network { 'plex-network':
+    ensure      => 'present',
+    driver      => 'bridge',
+    ipam_driver => 'default',
+    subnet      => '172.16.101.0/24',
+    gateway     => '172.16.101.1',
+    ip_range    => '172.16.101.0/24'
+  }
+
+  docker_volume { 'plex-volume':
+    ensure => present,
+  }
+
+  docker::image { 'plex':
+    image     => 'plexinc/pms-docker',
+    image_tag => 'latest'
+
+  }
+  docker::run { 'plex':
+    image           => 'plexinc/pms-docker:latest',
+    ports           => ['32400:32400','3005:3005','8324:8324','32469:32469','1900:1900','8843:8843','32410:32410','32412:32412','32413:32413','32414:32414'],
+    volumes         => ['plex-volume:/config', 'plex-volume:/transcode', 'plex-volume:/data'],
+    net             => 'plex-network',
+    restart_service => true,
+    pull_on_start   => false,
+    docker_service  => true,
+  }
 }
