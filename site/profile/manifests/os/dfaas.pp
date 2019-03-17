7d9bb2252d15573a665a8f4e9dfc4c2b105735b8
Chris Roberson <chris.roberson@puppet.com>
2019-03-17 12:37:06 -0600

    Need to check for install.

--- site/profile/manifests/os/dfaas.pp
+++ site/profile/manifests/os/dfaas.pp
@@ -34,7 +34,7 @@ class profile::os::dfaas (
   }
 
   -> package { 'JumpCloud v1.0':
-    ensure          => '0.10.0',
+    ensure          => installed,
     source          => 'C:\\Install\\JumpCloudInstaller.exe',
     install_options => [ '-k', $jcinstalluuid, '/SUPPRESSMSGBOXES', '/VERYSILENT', '/NORESTART'],
     notify          => Reboot['dfaas_reboot'],
