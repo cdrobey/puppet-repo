db77ee156e82f6a770d42548adfbfdf342fd54ef
Chris Roberson <cdroberson@gmail.com>
2019-09-23 16:12:35 -0600

    simplify networking

--- site/profile/manifests/apps/docker.pp
+++ site/profile/manifests/apps/docker.pp
@@ -15,21 +15,10 @@ class profile::apps::docker(
     version   => 'latest',
   }
 
-  docker_network { $private:
-    ensure      => 'present',
-    driver      => 'bridge',
-    ipam_driver => 'default',
-    subnet      => '172.16.100.0/24',
-    gateway     => '172.16.100.1',
-    ip_range    => '172.16.100.0/24'
-  }
+
   docker_network { $public:
-    ensure      => 'present',
-    driver      => 'bridge',
-    ipam_driver => 'default',
-    subnet      => '172.16.200.0/24',
-    gateway     => '172.16.200.1',
-    ip_range    => '172.16.200.0/24'
+    ensure => 'present',
+    driver => 'bridge',
   }
   include profile::apps::docker::unifi
   include profile::apps::docker::portainer
